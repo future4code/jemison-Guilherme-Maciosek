1b88cd5fa3904d22923643d38f7ad92f1ec30b60
unknown <glmaciosek@gmail.com>
2022-05-03 23:36:59 -0300

    entrega 03/05

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,139 @@
+/*----------------------------------------------------------------------*/
+//1. O código irá somar de 1 em 1 enquanto a variável valor for menor que 5. Será impresso também em console.
+/*----------------------------------------------------------------------*/
+//2. O código irá verificar todos os itens do array e se o número for maior que 18 será impresso em console.
+//2.A = 19, 21, 23, 25, 27, 30
+//2.B = É o suficiente para acessar o índice uma vez que ele percorre todos os itens do array.
+/*----------------------------------------------------------------------*/
+//3. Será impresso:
+//*
+//**
+//***
+//****
+/*----------------------------------------------------------------------*/
+
+// let quantidadeDeBichinhos = +prompt ("Quantos bichinhos você tem?")
+// let loopNomePet = 0
+
+// if (quantidadeDeBichinhos === 0) {
+//     console.log("Que pena! Você pode adotar um pet!")
+// } else {
+//     while (loopNomePet < quantidadeDeBichinhos) {
+//         let nomePets =[]; {                       
+//         nomePets = prompt ("Insira o nome dos pets")
+//     }
+//     loopNomePet++
+//     console.log(nomePets)    
+//     } 
+// }
+
+/**----------------------------------------------------------------------- */
+// var array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+// function imprimeArray(array){
+//     let numero = []
+//     for (let item of array){
+//         numero = numero + item +", "        
+//     }
+// console.log(numero)
+// }
+
+// imprimeArray(array)
+/**----------------------------------------------------------------------- */
+
+
+// var array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+// function imprimeArrayDividido(array){
+//     let numero = []
+//     for (let item of array){
+//         numero = numero + item/10 +", "       
+//     }
+// console.log(numero)
+// }
+
+// imprimeArrayDividido(array)
+/**----------------------------------------------------------------------- */
+
+
+// var array = []
+// var numerosPares =[]
+
+// function imprimeArrayPares(novoArray){
+//         for (let numerosArray of novoArray) {
+//         if(numerosArray % 2 === 0) {
+//         numerosPares.push(numerosArray)
+//         }           
+//     }
+//     console.log(numerosPares)     
+// } 
+// imprimeArrayPares(array)
+
+
+/**----------------------------------------------------------------------- */
+
+
+// var array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+
+// function imprimeArrayIndex(array) {
+//     let novoArray = []
+//     for (i = 0; i <array.length; i++) {
+//        novoArray.push (console.log(`O elemento do índex ${i} é ${array[i]}`))
+//     }
+// }
+
+// imprimeArrayIndex(array)
+
+
+/**----------------------------------------------------------------------- */
+
+// var array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+// function maiorEMenor(array) {
+    
+//     let menorNumero = ""
+//     for(let numero of array)
+//     if (numero > menorNumero){
+//         menorNumero  = numero;
+//     }       
+    
+    
+//     let maiorNumero = ""
+    
+//     for(let numero of array)
+//     if (maiorNumero > numero){
+//         numero = maiorNumero;
+//     }
+
+       
+// console.log(`O maior número é  ${menorNumero} `)
+// }
+
+// maiorEMenor(array)
+
+
+
+// var array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+// function agoraVai(array){
+//     let maiorNumero = ""
+//     let menorNumero = 
+//     for (let item of array)
+//     if (item > maiorNumero){
+//         maiorNumero = item;{
+//             if (item < menorNumero){
+//                 menorNumero = item
+//             }
+//         }
+//     }
+// console.log(`O maior número é  ${menorNumero} `)
+// }
+
+// agoraVai(array)
+
+
+
+//NÃO CONSEGUI FAZER//
+/**----------------------------------------------------------------------- */
+
