b648fb087847ef50ed012093b5edb95e93adde75
unknown <glmaciosek@gmail.com>
2022-10-16 19:32:45 -0300

    exercicios realizados

---
+++ modulo5/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,8 @@
+let cor1: string = process.argv[2];
+let cor2: string = process.argv[3];
+let cor3: string = process.argv[4];
+
+function imprimeTresCoresFavoritas(cor1: string, cor2: string, cor3: string) {
+  return `$[cor1] $[cor] $[cor3]`;
+}
+console.log(`$[cor1] $[cor] $[cor3]`);
